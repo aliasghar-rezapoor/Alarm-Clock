34f26dc49d4decff3a6d2a2faca5fa78c3cf4098
Aliasghar <aliasgharrezapoor456u@gmail.com>
2024-02-24 01:29:50 +0330

    commit 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const selectMenu = document.querySelectorAll('select');
+for(let i = 23 ; i >= 0 ; i--){
+    i = i < 10 ? '0' + i : i;
+    let option = `<option value="${i}">${i}</option>`
+    console.log(option)
+    selectMenu[0].firstElementChild.insertAdjacentHTML('afterend' , option)
+}
